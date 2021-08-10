22fd075f74c396189a39b39c8dc09438e3c2a54d
Ilya Beregovskiy <festeloqq@gmail.com>
2021-08-11 00:36:10 +0300

    update mxc logic

--- logic/lib/src/data/api/api.dart
+++ logic/lib/src/data/api/api.dart
@@ -1,11 +1,13 @@
 import 'package:chopper/chopper.dart';
 
+import 'package:meta/meta.dart';
 import 'error_converter.dart';
 import 'supernode/client_index.dart';
 
 export 'supernode/client_index.dart';
 export 'supernode/supernode.swagger.dart';
 
+@internal
 ChopperClient createClient(String baseUrl) {
   return ChopperClient(
     baseUrl: baseUrl,
