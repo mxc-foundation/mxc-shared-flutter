535f3c7fc070da95be092c3419f1a6d63ebfa1ae
Ilya Beregovskiy <festeloqq@gmail.com>
2021-08-18 13:47:05 +0300

    add secure.dart

---
+++ logic/test/secure.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+import 'package:dotenv/dotenv.dart' show load, env;
+
+String get supernodeAddress => env['SUPERNODE_ADDRESS']!;
+String get login => env['LOGIN']!;
+String get password => env['PASSWORD']!;
+
+void initSecure() => load(envFile);
+
+String get dirPath {
+  final fullPathSegments = Platform.script.pathSegments;
+  final folderPathSegments =
+      fullPathSegments.take(fullPathSegments.length - 1).toList();
+  // adds support to run flutter test from logic dir, not only from logic/test
+  if (folderPathSegments.last != "test") {
+    folderPathSegments.add("test");
+  }
+  return "/${folderPathSegments.join("/")}";
+}
+
+String get envFile {
+  return "$dirPath/.env";
+}
