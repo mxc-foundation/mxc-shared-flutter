d1622ac771a17bbd4a81b4c3a5517d4432c44a2b
festelo <festeloqq@gmail.com>
2021-08-23 16:15:06 +0000

    Deploying to gh-pages from  @ 7e82481b41ab34118a7fc0b0304c7fdfbef1ae2d 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -477,8 +477,8 @@ const RESOURCES = {
 "manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b",
 "version.json": "4b6db237b3514a88107a422469adfb0f",
 "main.dart.js": "41a987f6a7fca5538add2816760ab410",
-"index.html": "997092f1f2cec9156f9389d2cfde772f",
-"/": "997092f1f2cec9156f9389d2cfde772f"
+"index.html": "cedc2a4f0a3f32177bc38729a619d31b",
+"/": "cedc2a4f0a3f32177bc38729a619d31b"
 };
 
 // The application shell files that are downloaded before a service worker can
