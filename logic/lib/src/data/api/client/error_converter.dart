ddadb0a9efcda5b36b29dfa838a59e58c8daad82
Ilya Beregovskiy <festeloqq@gmail.com>
2021-08-24 17:47:37 +0300

    improve error converter

--- logic/lib/src/data/api/client/error_converter.dart
+++ logic/lib/src/data/api/client/error_converter.dart
@@ -1,27 +1,46 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:chopper/chopper.dart';
 
 class ChopperErrorConverter extends ErrorConverter {
   @override
   FutureOr<Response> convertError<BodyType, InnerType>(Response response) {
-    final error = response.error;
+    late final Object? error;
+    try {
+      error = jsonDecode(response.bodyString);
+    } on FormatException {
+      error = null;
+    }
     if (error is Map<String, dynamic>) {
       throw ApiException(
+        response.base.request?.url,
         (error['message'] ?? error['error'] ?? 'Unknown error') as String,
+        error,
       );
     }
     if (response.statusCode == 404) {
       throw ApiException(
-        'Resource not found: ${response.base.request?.url.toString()}',
+        response.base.request?.url,
+        'Resource not found.',
+        error,
       );
     }
-    throw ApiException('Unknown error');
+    throw ApiException(
+      response.base.request?.url,
+      'Unknown error. ${response.bodyString}',
+      error,
+    );
   }
 }
 
 class ApiException implements Exception {
-  final String error;
+  final Uri? url;
+  final String message;
+  final Object? source;
 
-  ApiException(this.error);
+  ApiException(this.url, this.message, this.source);
+
+  @override
+  String toString() => '$message for $url';
 }
