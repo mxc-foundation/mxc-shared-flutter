4b65db368fc1950fab4ffeea6107158bf571642e
miljan-mxc <72385784+miljan-mxc@users.noreply.github.com>
2021-10-07 17:44:20 +0200

    BtcLock and BtcMiningSession classes

--- logic/lib/src/domain/entities/wallet.dart
+++ logic/lib/src/domain/entities/wallet.dart
@@ -1,11 +1,21 @@
 class BtcMiningSession {
   final String sessionId;
-  final String mxcLockAmount;
-  final String mxcLockDurationDays;
+  final int mxcLockAmount;
+  final int mxcLockDurationDays;
 
   BtcMiningSession({
     required this.sessionId,
     required this.mxcLockAmount,
     required this.mxcLockDurationDays,
+  });
+}
+
+class BtcLock {
+  final String gatewayMac;
+  final String sessionId;
+
+  BtcLock({
+    required this.gatewayMac,
+    required this.sessionId,
   });
 }
