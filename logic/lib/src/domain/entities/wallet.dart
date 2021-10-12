3644292443ae2fa889d7698eb0c4cdb1abf76619
miljan-mxc <72385784+miljan-mxc@users.noreply.github.com>
2021-10-12 18:36:45 +0200

    Parse btc mining session start and end date

--- logic/lib/src/domain/entities/wallet.dart
+++ logic/lib/src/domain/entities/wallet.dart
@@ -1,11 +1,15 @@
 class BtcMiningSession {
   final String sessionId;
   final int mxcLockAmount;
+  final DateTime startSession;
+  final DateTime endSession;
   final int mxcLockDurationDays;
 
   BtcMiningSession({
     required this.sessionId,
     required this.mxcLockAmount,
+    required this.startSession,
+    required this.endSession,
     required this.mxcLockDurationDays,
   });
 }
