7d09655f39945f760d108b4ec5fea2ff4b65413f
rtviwe <rtviwe@gmail.com>
2021-11-02 14:10:03 +0300

    add ISRG_X1

--- logic/lib/src/data/data.dart
+++ logic/lib/src/data/data.dart
@@ -3,4 +3,5 @@ export 'cache/cache_field.dart';
 export 'cache/cache_manager.dart';
 export 'cache/cache_store.dart';
 
-String sembastDbName = 'best.db';
+const sembastDbName = 'best.db';
+const sslUrl = 'https://valid-isrgrootx1.letsencrypt.org/';
