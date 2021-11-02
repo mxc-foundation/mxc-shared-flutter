6842c6c09b44b7eb52bc120b1a06f1cd906a3971
rtviwe <rtviwe@gmail.com>
2021-11-02 14:43:53 +0300

    fix cert

--- logic/lib/src/data/api/client/client.dart
+++ logic/lib/src/data/api/client/client.dart
@@ -1,13 +1,50 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:chopper/chopper.dart';
+import 'package:http/io_client.dart';
 import 'package:meta/meta.dart';
 import 'package:mxc_logic/src/data/api/client/authentication.dart';
 
-import '../supernode/client_index.dart';
 import '../supernode/supernode.swagger.dart';
 import 'error_converter.dart';
 
 @internal
 class SupernodeClient extends ChopperClient {
+  // ignore: leading_newlines_in_multiline_strings
+  static const String isrgx1 = '''-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----''';
+
   final String Function() _getBaseUrl;
 
   SupernodeClient({
@@ -18,6 +55,7 @@ class SupernodeClient extends ChopperClient {
         super(
           converter: JsonSerializableConverter(),
           services: [...supernodeServices],
+          client: IOClient(_customHttpClient(cert: isrgx1)),
           authenticator: refreshToken == null
               ? null
               : SupernodeAuthenticator(
@@ -30,6 +68,26 @@ class SupernodeClient extends ChopperClient {
           ],
           errorConverter: ChopperErrorConverter(),
         );
+
+  static HttpClient _customHttpClient({String? cert}) {
+    final context = SecurityContext.defaultContext;
+    try {
+      if (cert != null) {
+        final bytes = utf8.encode(cert);
+        context.setTrustedCertificatesBytes(bytes);
+      }
+      log('createHttpClient() - cert added!');
+    } on TlsException catch (e) {
+      final fuck = e.osError?.message.contains('CERT_ALREADY_IN_HASH_TABLE') ?? false;
+      if (e.osError?.message != null && fuck) {
+        log('createHttpClient() - cert already trusted! Skipping.');
+      } else {
+        log('createHttpClient().setTrustedCertificateBytes EXCEPTION: $e');
+        rethrow;
+      }
+    } finally {}
+    return HttpClient(context: context);
+  }
 
   @override
   String get baseUrl => _getBaseUrl();
--- logic/lib/src/data/data.dart
+++ logic/lib/src/data/data.dart
@@ -2,4 +2,5 @@ export 'api/api.dart';
 export 'cache/cache_field_manager.dart';
 export 'cache/cache_manager.dart';
 
-String sembastDbName = 'best.db';
+const sembastDbName = 'best.db';
+const sslUrl = 'https://valid-isrgrootx1.letsencrypt.org/';
