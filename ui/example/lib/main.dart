47b28b0a7e3c39ae0920a18667a2d28b2296c551
miljan-mxc <72385784+miljan-mxc@users.noreply.github.com>
2022-03-28 14:28:45 +0200

    Revert "Remove example from logic and ui shred library"
    This reverts commit 4314930aaf934ecfa078fb21ba3ae41782ef3f9a.

---
+++ ui/example/lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:example/app_bar.dart';
+import 'package:example/colors.dart';
+import 'package:example/input.dart';
+import 'package:flutter/material.dart';
+import 'package:mxc_ui/mxc_ui.dart';
+
+import 'buttons.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final mxcThemeData = MxcThemeData.fromOption(MxcThemeOption.day);
+    return MxcTheme(
+      data: mxcThemeData,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: mxcThemeData.toThemeData(),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late final TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFE5E5E5),
+      body: TabBarView(
+        controller: controller,
+        physics: const NeverScrollableScrollPhysics(),
+        children: const [
+          ColorsPage(),
+          InputPage(),
+          AppBarPage(),
+          ButtonsPage(),
+        ],
+      ),
+      floatingActionButton: Container(
+        decoration: BoxDecoration(
+          color: Colors.amber.shade800,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              onPressed: () => controller.animateTo(controller.index - 1),
+              icon: Icon(Icons.arrow_back, color: Colors.grey.shade100),
+            ),
+            IconButton(
+              onPressed: () => controller.animateTo(controller.index + 1),
+              icon: Icon(Icons.arrow_forward, color: Colors.grey.shade100),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ ui/example/lib/utils.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AlwaysFocusedNode extends FocusNode {
+  @override
+  bool get hasFocus => true;
+}
