d647634c7f41f90e17bf0dd5b3ac01750973b152
Ilya Beregovskiy <festeloqq@gmail.com>
2022-03-28 09:58:48 +0200

    MMD-2452

--- logic/lib/src/domain/repositories/supernode/api/registration.dart
+++ logic/lib/src/domain/repositories/supernode/api/registration.dart
@@ -53,7 +53,7 @@ class RegistrationRepository {
       id: res.body!.id!,
       isAdmin: res.body!.isAdmin.orDefault(),
       isActive: res.body!.isActive.orDefault(),
-      token: res.body!.authToken!,
+      token: res.body!.jwt ?? res.body!.authToken!,
       username: res.body!.username!,
     );
   }
