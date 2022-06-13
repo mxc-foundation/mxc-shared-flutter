ceb5a98cab4aee46061dd7666cc7b02ceefdd557
miljan-mxc <72385784+miljan-mxc@users.noreply.github.com>
2022-06-13 14:30:20 +0200

    Add _MxcCircleSvgButton as MxcCircleButton subclass

--- ui/lib/src/buttons/circle_button.dart
+++ ui/lib/src/buttons/circle_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mxc_ui/mxc_ui.dart';
 
 abstract class MxcCircleButton extends StatelessWidget {
@@ -38,6 +39,16 @@ abstract class MxcCircleButton extends StatelessWidget {
     TextStyle? titleStyle,
   }) = _MxcCircleIconButton;
 
+  const factory MxcCircleButton.svg({
+    required Key? key,
+    required String svgString,
+    required VoidCallback? onTap,
+    bool filled,
+    String? title,
+    Color? color,
+    TextStyle? titleStyle,
+  }) = _MxcCircleSvgButton;
+
   final bool filled;
   final VoidCallback? onTap;
   final String? title;
@@ -47,7 +58,7 @@ abstract class MxcCircleButton extends StatelessWidget {
   Color _contentColor(BuildContext context) {
     Color color;
     if (onTap == null) {
-      color = ColorsTheme.of(context).textLabel;
+      color = ColorsTheme.of(context).buttonDisabledLabel;
     } else if (filled) {
       color = ColorsTheme.of(context).white;
     } else {
@@ -62,7 +73,7 @@ abstract class MxcCircleButton extends StatelessWidget {
   Widget build(BuildContext context) {
     Color fillColor;
     if (onTap == null) {
-      fillColor = ColorsTheme.of(context).primaryBackground;
+      fillColor = ColorsTheme.of(context).buttonDisabledBackground;
     } else if (filled) {
       fillColor = color ?? MxcScopedTheme.of(context).primaryColor;
     } else {
@@ -178,6 +189,35 @@ class _MxcCircleIconButton extends MxcCircleButton {
   @override
   Widget buildChild(BuildContext context) => Icon(
         icon,
+        color: _contentColor(context),
+      );
+}
+
+class _MxcCircleSvgButton extends MxcCircleButton {
+  const _MxcCircleSvgButton({
+    required Key? key,
+    required this.svgString,
+    required VoidCallback? onTap,
+    bool filled = false,
+    String? title,
+    Color? color,
+    TextStyle? titleStyle,
+  }) : super._(
+          key: key,
+          filled: filled,
+          onTap: onTap,
+          title: title,
+          color: color,
+          titleStyle: titleStyle,
+        );
+
+  final String svgString;
+
+  @override
+  Widget buildChild(BuildContext context) => SvgPicture.asset(
+        svgString,
+        height: 24,
+        width: 24,
         color: _contentColor(context),
       );
 }
